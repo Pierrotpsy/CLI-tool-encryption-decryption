3c04e507c9bc2233d5604c96f8967370261006df
Maxime Merlin <67154936+Pierrotpsy@users.noreply.github.com>
2023-02-26 19:46:03 +0100

    Start asymmetric implementation

---
+++ Asymmetric/lib/decryption.js
@@ -0,0 +1,16 @@
+const crypto = require("crypto")
+
+function decrypt(data, privateKey) {
+    const decryptedData = crypto.privateDecrypt(
+        {
+            key: privateKey,
+            // In order to decrypt the data, we need to specify the
+            // same hashing function and padding scheme that we used to
+            // encrypt the data in the previous step
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            oaepHash: "sha256",
+        },
+        data
+    );
+    return decryptedData.toString();
+}
